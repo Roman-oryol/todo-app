f81e4a928163b1b7a7527a96c88ab407c26059f7
Roman Oryol <roman.oryol83@gmail.com>
2026-07-16 16:33:24 +0300

    chore: добавлен eslint-plugin-react для улучшения линтинга React-компонентов

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import prettier from 'eslint-config-prettier';
 
@@ -13,6 +14,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      react.configs.flat.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,8 +25,18 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': [
+        'error',
+        { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+      ],
+      'no-undef': 'error',
+      'react/react-in-jsx-scope': 'off',
     },
   },
   prettier,
